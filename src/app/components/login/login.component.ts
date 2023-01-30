754dbdcd65a785020e20506324691f39a774b0a1
Alfonsina <yesicaguidounla@hotmail.com>
2023-01-30 06:33:55 -0300

    finalizo proyecto con vista responsive

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,7 +8,7 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  tipoDocumentoField = new FormControl('DNI', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]);
+  tipoDocumentoField = new FormControl('DNI', [Validators.required, Validators.minLength(2), Validators.maxLength(10), Validators.pattern(/^[a-zA-Z ]*$/)]);
   numeroDocumentoField = new FormControl('45888967', [Validators.required, Validators.pattern(/^[0-9]{7,10}$/)]);
   passwordField = new FormControl('Clave@01', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]/), Validators.minLength(5), Validators.maxLength(10)]);
 
